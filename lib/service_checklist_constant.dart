37ab03de252c342a7351ea70e835afa0cd3f1afc
Aditi Dixit <aditidixitn07@gmail.com>
2025-06-08 21:51:06 +0530

    Made minor changes and seperated UI

--- lib/service_checklist_constant.dart
+++ lib/service_checklist_constant.dart
@@ -16,19 +16,24 @@ class ServiceChecklistItems {
   static List<ServiceChecklistItem> getChecklistLabels(BuildContext context) {
     return [
       ServiceChecklistItem(
-          label: AppLocalizations.of(context)!.batteryUPSCleaned,),
+        label: AppLocalizations.of(context)!.batteryUPSCleaned,
+      ),
       ServiceChecklistItem(label: AppLocalizations.of(context)!.blowerChanged),
       ServiceChecklistItem(
-          label: AppLocalizations.of(context)!.upsConnectionTight,),
+        label: AppLocalizations.of(context)!.upsConnectionTight,
+      ),
       ServiceChecklistItem(
-          label: AppLocalizations.of(context)!.batteryWaterLevel,),
+        label: AppLocalizations.of(context)!.batteryWaterLevel,
+      ),
       ServiceChecklistItem(label: AppLocalizations.of(context)!.coolingFan),
       ServiceChecklistItem(label: AppLocalizations.of(context)!.backUpTest),
       ServiceChecklistItem(label: AppLocalizations.of(context)!.staticBypass),
       ServiceChecklistItem(
-          label: AppLocalizations.of(context)!.panelReadingMatched,),
+        label: AppLocalizations.of(context)!.panelReadingMatched,
+      ),
       ServiceChecklistItem(
-          label: AppLocalizations.of(context)!.historyCardEntry,),
+        label: AppLocalizations.of(context)!.historyCardEntry,
+      ),
       ServiceChecklistItem(label: AppLocalizations.of(context)!.equipmentOk),
     ];
   }

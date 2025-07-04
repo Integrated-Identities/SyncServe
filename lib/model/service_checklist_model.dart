2cec1979c6d9a8584eaebe63101461c8ac283eac
Aditi Dixit <aditidixitn07@gmail.com>
2025-07-04 22:24:17 +0530

    Removed the boolean isChecked

--- lib/model/service_checklist_model.dart
+++ lib/model/service_checklist_model.dart
@@ -3,15 +3,11 @@ import 'package:syncserve/enums/service_checklist.dart';
 class ServiceChecklistItem {
   ServiceChecklistItem({
     required this.label,
-    this.isChecked = false,
     required this.flag,
   });
 
   /// The label for the checklist item.
   final String label;
-
-  /// Has the user completed this checklist item?
-  bool isChecked;
 
   final ServiceChecklistFlag flag;
 }

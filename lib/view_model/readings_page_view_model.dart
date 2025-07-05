846d5983bf465983bc48fb8535327c864f30af07
Aditi Dixit <aditidixitn07@gmail.com>
2025-07-05 13:55:49 +0530

    Added Riverpod to the readings_page

--- lib/view_model/readings_page_view_model.dart
+++ lib/view_model/readings_page_view_model.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:syncserve/enums/battery_type.dart';
+import 'package:syncserve/model/readings_page_model.dart';
+import 'package:syncserve/providers/readings_page_providers.dart';
 
 enum PhaseType { onePhase, threePhase }
 
 class ReadingsViewModel {
+  void save(WidgetRef ref) {
+    final model = ReadingsPageModel(
+      power: powerController.text.trim(),
+      dcVoltage: dcVoltageController.text.trim(),
+      energy: energyController.text.trim(),
+      manufactureName: manufacturerNameController.text.trim(),
+      withStabilizer: withStabilizer,
+      quantity: quantity,
+      inputPhase: inputPhase,
+      outputPhase: outputPhase,
+      batteryType: batteryType,
+    );
+    ref.read(readingsPageProvider.notifier).state = model;
+  }
+
   final powerController = TextEditingController();
   final dcVoltageController = TextEditingController();
   final energyController = TextEditingController();

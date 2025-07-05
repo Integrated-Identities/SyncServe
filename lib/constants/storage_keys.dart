470dacd64ee772f791b3a47478feafd8a107776a
Shantanu Methikar <smethikar18@gmail.com>
2025-07-05 20:02:51 +0530

    Squashed commit of the following:
    commit eafcb87132d43bb6d3e1a382d7c99c5220d6d77d
    Author: Aditi <aditidixitn07@gmail.com>
    Date:   Sat Jul 5 19:26:24 2025 +0530
    
        [SS-45] Non-secure storage  (#24)
    
        * Added non-secure storage with the help of riverpod provider
    
        * Removed the load method from view_model
    
        * Resolved the requested comments
    
        * Resolved the requested comments
    
    commit ebaa29f9840dddd9613603858a4dbf33f0a44bb1
    Author: Aditi <aditidixitn07@gmail.com>
    Date:   Fri Jul 4 23:12:28 2025 +0530
    
        [SS-48] Employee_details page (#27)
    
        * Added a new page called as employee details
    
        * Removed the Exception
    
        * Resolved the requested changes
    
    commit 460f7ac4ce74a199fd48e17d5257def35ea4c1d7
    Author: Aditi <aditidixitn07@gmail.com>
    Date:   Fri Jul 4 22:26:19 2025 +0530
    
        Bit flags for service_checklist (#28)
    
        * Added bit flags for service_checklist page
    
        * Removed the boolean isChecked

---
+++ lib/constants/storage_keys.dart
@@ -0,0 +1,8 @@
+class StorageKeys {
+  const StorageKeys._();
+
+  static const String customerName = 'customer_name';
+  static const String customerEmail = 'customer_email';
+  static const String customerAddress = 'customer_address';
+  static const String customerPhone = 'customer_phone';
+}
--- lib/providers/customer_providers.dart
+++ lib/providers/customer_providers.dart
@@ -1,5 +1,16 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:syncserve/model/customer.dart';
+import 'package:syncserve/model/customer_detail_model.dart';
+import 'package:syncserve/services/non_secure_storage_service.dart';
+import 'package:syncserve/view_model/customer_details_view_model.dart';
 
 // Riverpod provider to hold selected customer details throughout the app
 final customerProvider = StateProvider<Customer?>((ref) => null);
+
+final customerDetailsViewModelProvider = Provider<CustomerDetailsViewModel>(
+  (ref) => CustomerDetailsViewModel(),
+);
+
+final nonSecureStorageProvider =
+    FutureProvider<NonSecureStorageService>((ref) async {
+  return await NonSecureStorageService.create();
+});
